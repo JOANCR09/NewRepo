5f7f8fccfb0b944f59bad05b95fb01bff4ad9165
JOANCR09 <60800028+JOANCR09@users.noreply.github.com>
2022-12-19 09:53:18 -0600

    Agregar archivos de proyecto.

---
+++ OOP-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Evans", 1000);
+            Console.WriteLine($"Nombre {account.Owner}, numero de cueta: {account.Number} y su credito es de: {account.Balance}");      
+            Console.ReadLine();
+        }
+    }
+}

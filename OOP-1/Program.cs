57bd048c0754f2e9206cabb90d919e8bfec8ee9e
JOANCR09 <60800028+JOANCR09@users.noreply.github.com>
2022-12-19 10:30:04 -0600

    change in method bank and program

--- OOP-1/Program.cs
+++ OOP-1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,30 @@ namespace OOP_1
         {
             var account = new BankAccount("Evans", 1000);
             Console.WriteLine($"Nombre {account.Owner}, numero de cueta: {account.Number} y su credito es de: {account.Balance}");      
+            
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine("su saldo es: "+account.Balance);
+
+            Console.WriteLine($"Nombre {account.Owner}, numero de cueta: {account.Number} y su credito es de: {account.Balance}");
+            Console.ReadLine();
+
+            // Test that the initial balances must be positive.
+            //BankAccount invalidAccount;
+            //try
+            //{
+            //    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            Console.WriteLine(account.GetAccountHistory());
+
             Console.ReadLine();
         }
     }
